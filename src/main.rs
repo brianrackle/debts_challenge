6ff86b52a35ffc34ecfe2b8cfc598135012d4c83
brianrackle <N8$N#B!&mdwO>
2021-03-14 14:46:05 -0700

    linear transactions balancing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use rand::Rng;
-use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt};
 
 fn gen_order_groups(people_count: usize, meal_count: usize) -> Vec<Vec<(u8, bool)>> {
     let mut rng = rand::thread_rng();
@@ -20,7 +18,6 @@ fn gen_order_groups(people_count: usize, meal_count: usize) -> Vec<Vec<(u8, bool
 }
 
 fn gen_balances(people_count: usize, meal_count: usize) -> Vec<i64> {
-    //dont need two values can just .0 - .1 = balance
     let mut order_sums = vec![0i64; people_count];
 
     let order_groups = gen_order_groups(people_count, meal_count);
@@ -38,52 +35,35 @@ fn gen_balances(people_count: usize, meal_count: usize) -> Vec<i64> {
 }
 
 //each transaction balances one side to zero
-fn balance_transactions(balances : &mut Vec<i64>, i: usize, j: usize) {
-    if balances[i] >= balances[j] {
-        balances[i] += balances[j];
-        balances[j] = 0;
-    } else {
-        balances[j] += balances[i];
-        balances[i] = 0;
+fn balance_first_transaction(balances: &mut Vec<i64>, i: usize, j: usize) {
+    balances[j] += balances[i];
+    balances[i] = 0;
+}
+
+fn linear_transaction_balancing(mut balances: Vec<i64>) -> usize {
+    let mut transactions = 0;
+    for i in 0..balances.len() - 1 {
+        if balances[i] != 0 {
+            let mut j = i+1;
+            while j < balances.len() && balances[j] == 0 {
+                j += 1;
+            }
+            if balances[j] != 0 {
+                balance_first_transaction(&mut balances, i, j);
+                transactions += 1;
+            }
+        }
     }
+    transactions
 }
 
-fn min_transaction_brute_force(balances: Vec<i64>) -> usize {
-        //start with p1 to p2
-    0usize
-}
-
-#[tokio::main]
-pub async fn main() -> io::Result<()> {
-    //generate random debts and credits
-    //iterate best possible solution (brute force)
-    //log the n value, minimum number of transactions, ratio transactions / n, set of transactions and data set
-
-    //let mut f = File::create("logs.txt").await?;
+pub fn main() {
     let mut rng = rand::thread_rng();
     for _ in 0..10 {
         let balances = gen_balances(20, rng.gen_range(30..300));
         println!("balances {:?}", balances);
-        println!("balances sum {:?}", balances.iter().sum::<i64>())
+        println!("min transactions: {:?}", linear_transaction_balancing(balances));
     }
-
-    // read up to 10 bytes
-    // let n = f.read(&mut buffer[..]).await?;
-    //
-    // println!("The bytes: {:?}", &buffer[..n]);
-    //
-    // // Open a connection to the mini-redis address.
-    // let mut client = client::connect("127.0.0.1:6379").await?;
-    //
-    // // Set the key "hello" with value "world"
-    // client.set("hello", "world".into()).await?;
-    //
-    // // Get key "hello"
-    // let result = client.get("hello").await?;
-    //
-    // println!("got value from the server; result={:?}", result);
-
-    Ok(())
 }
 
 #[cfg(test)]
@@ -116,49 +96,25 @@ mod tests {
 
     #[test]
     fn are_transactions_balanced_0_0() {
-        let mut balances = vec![0,0];
-        balance_transactions(&mut balances, 0, 1);
+        let mut balances = vec![0, 0];
+        balance_first_transaction(&mut balances, 0, 1);
         assert_eq!(balances[0], 0);
-        assert_eq!(balances[1], 0);
-    }
-
-    #[test]
-    fn are_transactions_balanced_8_n3() {
-        let mut balances = vec![8,-3];
-        balance_transactions(&mut balances, 0, 1);
-        assert_eq!(balances[0], 5);
-        assert_eq!(balances[1], 0);
-    }
-
-    #[test]
-    fn are_transactions_balanced_n3_8() {
-        let mut balances = vec![-3,8];
-        balance_transactions(&mut balances, 0, 1);
-        assert_eq!(balances[0], 0);
-        assert_eq!(balances[1], 5);
-    }
-
-    #[test]
-    fn are_transactions_balanced_3_n3() {
-        let mut balances = vec![3,-3];
-        balance_transactions(&mut balances, 0, 1);
-        assert_eq!(balances[0], 0);
-        assert_eq!(balances[1], 0);
-    }
-
-    #[test]
-    fn are_transactions_balanced_3_3() {
-        let mut balances = vec![3,3];
-        balance_transactions(&mut balances, 0, 1);
-        assert_eq!(balances[0], 6);
         assert_eq!(balances[1], 0);
     }
 
     #[test]
     fn are_transactions_balanced_3_4() {
-        let mut balances = vec![3,4];
-        balance_transactions(&mut balances, 0, 1);
+        let mut balances = vec![3, 4];
+        balance_first_transaction(&mut balances, 0, 1);
         assert_eq!(balances[0], 0);
         assert_eq!(balances[1], 7);
+    }
+
+    #[test]
+    fn are_transactions_balanced_n3_2() {
+        let mut balances = vec![-3, 2];
+        balance_first_transaction(&mut balances, 0, 1);
+        assert_eq!(balances[0], 0);
+        assert_eq!(balances[1], -1);
     }
 }
